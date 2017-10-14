0a8ff6620198ee2d2eea32cd87e2e62a95b4eb58
Marco Solazzi <marco.solazzi@gmail.com>
2017-10-14 22:29:37 +0200

    feat(eslint): base eslint config

---
+++ packages/eslint-config-umeboshi/index.js
@@ -0,0 +1,50 @@
+const { resolve, CONFIG_LOAD_PATHS } = require('umeboshi-scripts/lib/utils');
+
+const webpackConfPath = resolve('webpack/webpack.base.js', CONFIG_LOAD_PATHS);
+
+module.exports = {
+    parser: 'babel-eslint',
+
+    extends: ['eslint-config-airbnb-base'],
+
+    env: {
+        commonjs: true,
+        es6: true,
+        browser: true
+    },
+
+    settings: webpackConfPath ? {
+        'import/resolver': {
+            webpack: {
+                config: webpackConfPath
+            }
+        }
+    } : {},
+
+    globals: {
+        Modernizr: true,
+        __PRODUCTION__: true
+    },
+
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: false
+        }
+    },
+
+    rules: {
+        indent: [2, 4],
+        'no-multiple-empty-lines': [2, { max: 3 }],
+        'spaced-comment': 0,
+        'comma-dangle': [2, 'never'],
+        'eol-last': [1, 'never'],
+        'padded-blocks': [1, { classes: 'always' }],
+        'max-len': 0,
+        'no-underscore-dangle': 0,
+        'arrow-parens': 0,
+        'import/no-unresolved': [2, { ignore: ['[a-z]+.s?css$'] }]
+    }
+};
