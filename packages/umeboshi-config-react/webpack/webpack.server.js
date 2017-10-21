b3772fd76612c47d6f90358a67bfa2b1fd46ab37
Marco Solazzi <marco.solazzi@gmail.com>
2017-10-21 23:31:13 +0200

    feat(config): react config

---
+++ packages/umeboshi-config-react/webpack/webpack.server.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const config = require('umeboshi-config/webpack/webpack.server');
+
+const app = [
+    'eventsource-polyfill', // Necessary for hot reloading with IE
+    'react-hot-loader/patch',
+    ...config.entry.app.slice(1),
+    'webpack/hot/only-dev-server'
+];
+
+module.exports = Object.assign({}, config, {
+    entry: {
+        app
+    },
+
+    plugins: (config.plugins || []).concat([
+        new webpack.HotModuleReplacementPlugin()
+    ])
+});
