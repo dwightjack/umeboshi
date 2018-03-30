9a3a0e1e1aebcedc6ebfa84cd5bce83e6894da03
Marco Solazzi <marco.solazzi@gmail.com>
2018-03-30 14:50:25 +0900

    refactor(config): demand this feature to a script

--- packages/umeboshi-config/webpack/webpack.conf.js
+++
@@ -1,5 +0,0 @@
-const { loadConfig } = require('umeboshi-dev-utils');
-
-const PRODUCTION = process.env.NODE_ENV === 'production';
-
-module.exports = loadConfig(`webpack/webpack.${PRODUCTION ? 'prod' : 'dev'}.js`);
