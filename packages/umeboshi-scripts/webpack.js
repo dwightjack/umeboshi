ab7fbd00a0bc5b3ba268e0938370a44cb6b9aa51
Marco Solazzi <marco.solazzi@gmail.com>
2018-04-02 13:18:05 +0900

    feat(scripts): export webpack conf as a function

--- packages/umeboshi-scripts/webpack.js
+++ packages/umeboshi-scripts/webpack.js
@@ -1,17 +1,19 @@
-const { argv } = require('yargs');
 const { loadConfig, loadUmeboshiConfig, mergeConfig } = require('umeboshi-dev-utils');
 
-const analyze = !!argv.analyze;
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 const $loaders = loadConfig('webpack/loaders.js');
 const $plugins = loadConfig('webpack/plugins.js');
-const webpackConfig = analyze ? loadConfig('webpack/analyze.js') : loadConfig(`webpack/webpack.${PRODUCTION ? 'prod' : 'dev'}.js`);
 const umeWebpack = loadUmeboshiConfig('webpack');
 
-const config = mergeConfig(webpackConfig, umeWebpack, $loaders, $plugins);
+module.exports = (env) => {
 
-config.module.rules.push(...$loaders.toLoaders());
-config.plugins.push(...$plugins.toPlugins());
+    const webpackConfig = env && env.analyze ? loadConfig('webpack/webpack.analyze.js') : loadConfig(`webpack/webpack.${PRODUCTION ? 'prod' : 'dev'}.js`);
 
-module.exports = config;
+    const config = mergeConfig(webpackConfig, umeWebpack, env, $loaders, $plugins);
+
+    config.module.rules.push(...$loaders.toLoaders());
+    config.plugins.push(...$plugins.toPlugins());
+
+    return config;
+};
