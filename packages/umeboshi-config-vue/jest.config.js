9eaba07daa1f5291e5cdf03c16fbabf1b412994d
Marco Solazzi <marco.solazzi@gmail.com>
2018-04-25 17:32:12 +0900

    fix(config-vue): merge deep

--- packages/umeboshi-config-vue/jest.config.js
+++ packages/umeboshi-config-vue/jest.config.js
@@ -1,11 +1,12 @@
 const config = require('umeboshi-config/jest.config');
+const merge = require('lodash.merge');
 
-module.exports = Object.assign(config, {
+module.exports = merge(config, {
     moduleFileExtensions: ['js', 'json', 'vue'],
     setupFiles: [require.resolve('./lib/jest.setup')],
-    moduleNameMapper: {
+    moduleNameMapper: Object.assign({
         '^vue$': 'vue/dist/vue.min.js'
-    },
+    }, config.moduleNameMapper),
     transform: {
         '^.+\\.js$': 'babel-jest',
         '.*\\.(vue)$': 'jest-vue-preprocessor'
