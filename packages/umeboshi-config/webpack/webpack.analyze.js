86431d5e7b009301f9dc450b824398912cac3803
Marco Solazzi <marco.solazzi@gmail.com>
2018-04-25 10:59:20 +0900

    fix(config): fix analyze.webpack plugin syntax

--- packages/umeboshi-config/webpack/webpack.analyze.js
+++ packages/umeboshi-config/webpack/webpack.analyze.js
@@ -4,9 +4,9 @@ module.exports = (env) => {
     const config = require('./webpack.prod')(env);
 
     config.plugin('bundle-analyzer')
-        .use(BundleAnalyzerPlugin, {
+        .use(BundleAnalyzerPlugin, [{
             analyzerHost: '0.0.0.0'
-        });
+        }]);
 
     return config;
 
