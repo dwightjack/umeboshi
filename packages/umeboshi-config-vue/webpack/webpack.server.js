11368e0deb85cf8b261b656536731e8ac66c0151
Marco Solazzi <marco.solazzi@gmail.com>
2018-04-25 17:34:06 +0900

    refactor(config-vue): simplified config

--- packages/umeboshi-config-vue/webpack/webpack.server.js
+++ packages/umeboshi-config-vue/webpack/webpack.server.js
@@ -5,14 +5,8 @@ module.exports = (env, cfg) => {
 
     const config = serverConf(env, cfg);
 
-    //eventsource-polyfill must be at the beginning
-    const entries = config.entry('app').delete('eventsource-polyfill').values();
-
-    config.entry('app').clear().merge([
-        'eventsource-polyfill', // Necessary for hot reloading with IE
-        ...entries,
-        'webpack/hot/only-dev-server'
-    ]);
+    config.entry('app')
+    .add('webpack/hot/only-dev-server');
 
     config.devServer
         .hot(true);
