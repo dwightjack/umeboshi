e0bfa66b25b615f68b3c6b91d85054c85de683cd
Marco Solazzi <marco.solazzi@gmail.com>
2018-04-25 17:03:11 +0900

    fix(config): allow js path alias on jest

--- packages/umeboshi-config/jest.config.js
+++ packages/umeboshi-config/jest.config.js
@@ -22,6 +22,7 @@ module.exports = {
     ],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file-mock.js',
-        '\\.(css|scss)$': 'identity-obj-proxy'
+        '\\.(css|scss)$': 'identity-obj-proxy',
+        '^@[/](.+)': '<rootDir>/$1'
     }
 };
