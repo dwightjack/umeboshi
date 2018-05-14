22d2d684dfac322cb8a5dd9a238ef813f458ae91
Marco Solazzi <marco.solazzi@gmail.com>
2018-05-14 14:29:22 +0900

    feat(scripts): support multiple configs in webpack
    supports array of config objects (both plain and chain instances) both
    in conf packages and umeboshi.config.js

--- packages/umeboshi-scripts/webpack.js
+++ packages/umeboshi-scripts/webpack.js
@@ -1,8 +1,15 @@
 const { loadConfig, loadUmeboshiConfig, mergeConfig } = require('umeboshi-dev-utils');
-
+const isFunction = require('lodash/isFunction');
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 const umeWebpack = loadUmeboshiConfig('webpack');
+
+const toConfig = (config) => {
+    if (Array.isArray(config)) {
+        return config.map(toConfig);
+    }
+    return isFunction(config.toConfig) ? config.toConfig() : config;
+};
 
 module.exports = (env) => {
 
@@ -11,8 +18,10 @@ module.exports = (env) => {
     const packageConfig = webpackConfig(env);
 
     if (Array.isArray(packageConfig)) {
-        return packageConfig.map((c) => mergeConfig(c, umeWebpack, env).toConfig());
+        return packageConfig.reduce(
+            (a, c) => a.concat(toConfig(mergeConfig(c, umeWebpack, env, true))),
+            []
+        );
     }
-
-    return mergeConfig(packageConfig, umeWebpack, env).toConfig();
+    return toConfig(mergeConfig(packageConfig, umeWebpack, env));
 };
