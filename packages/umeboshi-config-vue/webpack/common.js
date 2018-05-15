58af77583d651324a30b51b45422514d5d156395
Marco Solazzi <marco.solazzi@gmail.com>
2018-05-15 15:18:34 +0900

    fix(config-vue): account for non-module vue style blocks

--- packages/umeboshi-config-vue/webpack/common.js
+++ packages/umeboshi-config-vue/webpack/common.js
@@ -2,17 +2,26 @@ const {
     css, scss, resolveUrl, postcss, addCSSRule, applyLoaders, umeStyles
 } = require('umeboshi-config/webpack/style-loaders');
 const { paths } = require('umeboshi-dev-utils');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 const createLoaderRule = (config, name, test, loaders = []) => {
     const baseCSSRule = addCSSRule(config, { name, test });
     const modulesCSSRule = baseCSSRule.oneOf('modules').resourceQuery(/module/);
+    const vueCSSRule = baseCSSRule.oneOf('vue-style').resourceQuery(/vue/);
     const normalCSSRule = baseCSSRule.oneOf('normal');
 
     applyLoaders(
         modulesCSSRule,
         [css({ modules: true }), postcss(), ...loaders],
+        PRODUCTION,
+        'vue-style-loader'
+    );
+
+    applyLoaders(
+        vueCSSRule,
+        [css({ modules: false }), postcss(), ...loaders],
         PRODUCTION,
         'vue-style-loader'
     );
@@ -49,15 +58,13 @@ module.exports = (config) => {
             .use('vue-loader')
                 .loader('vue-loader')
                 .options({
-                    preserveWhitespace: false,
-                    transformToRequire: {
-                        video: 'src',
-                        source: 'src',
-                        img: 'src',
-                        image: 'xlink:href'
+                    compilerOptions: {
+                        preserveWhitespace: false
                     }
                 });
     /* eslint-enable indent */
+    config.plugin('vue')
+        .use(VueLoaderPlugin);
 
     return config;
 };
