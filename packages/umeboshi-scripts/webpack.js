d3096a6f4cbe53ace7dd1e4af443cd0d4bbea404
Marco Solazzi <marco.solazzi@gmail.com>
2018-05-15 17:12:20 +0900

    fix: loading the wrong webpack conf with the analyzer
    when using a child config (-vue, -react ... ) we were always picking wepack.prod from
    umeboshi-config
    
    closes #18

--- packages/umeboshi-scripts/webpack.js
+++ packages/umeboshi-scripts/webpack.js
@@ -1,5 +1,7 @@
 const { loadConfig, loadUmeboshiConfig, mergeConfig } = require('umeboshi-dev-utils');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const isFunction = require('lodash/isFunction');
+const identity = require('lodash/identity');
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 const umeWebpack = loadUmeboshiConfig('webpack');
@@ -11,17 +13,25 @@ const toConfig = (config) => {
     return isFunction(config.toConfig) ? config.toConfig() : config;
 };
 
-module.exports = (env) => {
+module.exports = (env = {}) => {
 
-    const webpackConfig = env && env.analyze ? loadConfig('webpack/webpack.analyze.js') : loadConfig(`webpack/webpack.${PRODUCTION ? 'prod' : 'dev'}.js`);
+    const { analyze } = env;
+    const webpackConfig = loadConfig(`webpack/webpack.${PRODUCTION || analyze ? 'prod' : 'dev'}.js`);
 
+    const addAnalyzer = analyze ? (config) => {
+        config.plugin('bundle-analyzer')
+            .use(BundleAnalyzerPlugin, [{
+                analyzerHost: '0.0.0.0'
+            }]);
+        return config;
+    } : identity;
     const packageConfig = webpackConfig(env);
 
     if (Array.isArray(packageConfig)) {
         return packageConfig.reduce(
-            (a, c) => a.concat(toConfig(mergeConfig(c, umeWebpack, env, true))),
+            (a, c) => a.concat(toConfig(mergeConfig(addAnalyzer(c), umeWebpack, env, true))),
             []
         );
     }
-    return toConfig(mergeConfig(packageConfig, umeWebpack, env));
+    return toConfig(mergeConfig(addAnalyzer(packageConfig), umeWebpack, env));
 };
