5e8f6c3b8c73dc57a7870bdac93c922b3238200f
Marco Solazzi <marco.solazzi@gmail.com>
2018-05-16 10:37:23 +0900

    refactor: moved modernizr config to umeboshi-config
    production config can now be configured from a "customizr" key

---
+++ packages/umeboshi-config/modernizr/prod.js
@@ -0,0 +1,47 @@
+const { paths } = require('umeboshi-dev-utils');
+const { options } = require('./dev');
+
+module.exports = {
+
+    cache: true,
+
+    devFile: false,
+
+    dest: false,
+
+    // Based on default settings on http://modernizr.com/download/
+    options,
+
+    // By default, source is uglified before saving
+    uglify: true,
+
+    // Define any tests you want to explicitly include
+    tests: [
+        'pointerevents',
+        'touchevents'
+    ],
+
+    // Useful for excluding any tests that this tool will match
+    // e.g. you use .notification class for notification elements,
+    // but don’t want the test for Notification API
+    excludeTests: [],
+
+    // By default, will crawl your project for references to Modernizr tests
+    // Set to false to disable
+    crawl: true,
+
+    // Set to true to pass in buffers via the 'files" parameter below
+    useBuffers: false,
+
+    // By default, this task will crawl all *.js, *.css, *.scss files.
+    files: {
+        src: [
+            paths.toPath('src.assets/js') + '/**/*.{js,scss,css}',
+            '!' + paths.toPath('src.assets/js') + '/**/*.{spec,conf,test}.js',
+            paths.toPath('src.assets/styles') + '/**/*.{scss,css}'
+        ]
+    },
+
+    // Have custom Modernizr tests? Add them here.
+    customTests: []
+};
--- packages/umeboshi-scripts/modernizr.js
+++ packages/umeboshi-scripts/modernizr.js
@@ -1,57 +1,17 @@
 const crypto = require('crypto');
 const fs = require('fs');
+
 const {
     loadConfig, loadUmeboshiConfig, mergeConfig, paths
 } = require('umeboshi-dev-utils');
 
 const umeModernizr = loadUmeboshiConfig('modernizr');
+const umeCustomizr = loadUmeboshiConfig('customizr');
 
-const devConfig = mergeConfig(loadConfig('modernizr.conf.json'), umeModernizr);
+const devConfig = mergeConfig(loadConfig('modernizr/dev.js'), umeModernizr);
 
-const prodConfig = mergeConfig({
 
-    cache: true,
-
-    devFile: false,
-
-    dest: false,
-
-    // Based on default settings on http://modernizr.com/download/
-    options: devConfig.options,
-
-    // By default, source is uglified before saving
-    uglify: true,
-
-    // Define any tests you want to explicitly include
-    tests: [
-        'pointerevents',
-        'touchevents'
-    ],
-
-    // Useful for excluding any tests that this tool will match
-    // e.g. you use .notification class for notification elements,
-    // but don’t want the test for Notification API
-    excludeTests: [],
-
-    // By default, will crawl your project for references to Modernizr tests
-    // Set to false to disable
-    crawl: true,
-
-    // Set to true to pass in buffers via the 'files" parameter below
-    useBuffers: false,
-
-    // By default, this task will crawl all *.js, *.css, *.scss files.
-    files: {
-        src: [
-            paths.toPath('src.assets/js') + '/**/*.{js,scss,css}',
-            '!' + paths.toPath('src.assets/js') + '/**/*.{spec,conf,test}.js',
-            paths.toPath('src.assets/styles') + '/**/*.{scss,css}'
-        ]
-    },
-
-    // Have custom Modernizr tests? Add them here.
-    customTests: []
-}, umeModernizr);
+const prodConfig = mergeConfig(loadConfig('modernizr/prod.js'), umeCustomizr);
 
 const filePath = paths.toPath('dist.assets/vendors/modernizr');
 
