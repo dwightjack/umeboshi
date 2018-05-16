51f96a09c793c882efd2c048ed9e871e0a947822
Marco Solazzi <marco.solazzi@gmail.com>
2018-05-16 10:41:27 +0900

    fix(config-vue): customizr: parse vue files
    use buffers as input and supply raw vue SFC blocks
    
    closes #16

---
+++ packages/umeboshi-config-vue/modernizr/prod.js
@@ -0,0 +1,46 @@
+const glob = require('globby');
+const fs = require('fs');
+const Vinyl = require('vinyl');
+const { parse } = require('@vue/component-compiler-utils');
+const { paths } = require('umeboshi-dev-utils');
+
+const base = require('umeboshi-config/modernizr/prod');
+
+const toFile = (path, data) => new Vinyl({ path, contents: Buffer.from(data, 'utf8') });
+
+const commons = glob.sync([
+    paths.toPath('src.assets/js') + '/**/*.{js,scss,css}',
+    '!' + paths.toPath('src.assets/js') + '/**/*.{spec,conf,test}.js',
+    paths.toPath('src.assets/styles') + '/**/*.{scss,css}'
+]).map((file) => (
+    toFile(file, fs.readFileSync(file, { encoding: 'utf8' }))
+));
+
+//test vue files...
+const vue = glob.sync([
+    paths.toPath('src.assets/js') + '/**/*.vue'
+]).reduce((acc, file) => {
+    const source = fs.readFileSync(file, { encoding: 'utf8' });
+    const { styles = [], script } = parse({ source, needMap: false });
+    if (script) {
+        const { content, lang = 'js' } = script;
+        const filename = file.replace(/\.vue$/, `~vue.${lang}`);
+        acc.push(toFile(filename, content));
+    }
+    styles.forEach(({ content, lang = 'css' }, i) => {
+        if (content) {
+            const filename = file.replace(/\.vue$/, `~vue.${i}.${lang}`);
+            acc.push(toFile(filename, content));
+        }
+    });
+    return acc;
+
+}, []);
+
+
+module.exports = Object.assign(base, {
+    useBuffers: true,
+    files: {
+        src: [...commons, ...vue]
+    }
+});
