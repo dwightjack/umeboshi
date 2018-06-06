7f1492c3961dba8105da849a05f6762b7c8e3055
Marco Solazzi <marco.solazzi@gmail.com>
2018-06-06 16:27:24 +0900

    refactor(config-jamstack): resolve the module to load into the renderer

--- packages/umeboshi-config-jamstack/examples/react/render.js
+++ packages/umeboshi-config-jamstack/examples/react/render.js
@@ -3,7 +3,16 @@ import { renderToStaticMarkup } from 'react-dom/server';
 import { Helmet } from 'react-helmet';
 
 //eslint-disable-next-line import/prefer-default-export
-const render = ({ page }) => {
+const render = ({ path }) => {
+
+    let page = '';
+    if (ctx.path.endsWith('/')) {
+        page += `${ctx.path}/index.js`;
+    } else if (ctx.path.endsWith('.html')) {
+        page += ctx.path.replace(/\.html$/, '.js');
+    }
+
+    page = page.replace(/\/+/g, '/').replace(/^\//, '');
 
     let Component = require(`@/pages/${page}`).default; //eslint-disable-line global-require, import/no-dynamic-require
 
