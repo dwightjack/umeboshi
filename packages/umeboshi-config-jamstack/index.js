ea8882ce3036fb157f86996d8f67c17d0c706c34
Marco Solazzi <marco.solazzi@gmail.com>
2018-06-06 16:24:53 +0900

    feat(config-jamstack): package configuration

--- packages/umeboshi-config-jamstack/index.js
+++ packages/umeboshi-config-jamstack/index.js
@@ -1,23 +1,77 @@
 const {
     staticMiddleware
 } = require('umeboshi-config-spa/middlewares');
+const webpack = require('webpack');
 
 const proxy = require('koa-proxy');
+const once = require('lodash/once');
+const execa = require('execa');
 
-module.exports = (config) => {
+const noop = () => {};
+
+module.exports = (config, { port = 9000 }) => {
+
+    config.set('jamstack', { port });
+
+    const oldServe = config.get('onServe') || noop;
+
+    config.set('onServe', ({ paths }) => {
+
+        return (env, webpackConfig, clientCompiler) => {
+            oldServe(env, webpackConfig, clientCompiler);
+
+            clientCompiler.hooks.done('jamServerStart', once(() => {
+                execa('ume-jam-server', {
+                    env: {
+                        MANIFEST: paths.toAbsPath('dist.assets/manifest.json'),
+                        TARGET_ENV: 'node'
+                    },
+                    cwd: process.cwd(),
+                    stdio: ['inherit', 'inherit', 'inherit']
+                });
+            }));
+
+            return true;
+        };
+
+
+    });
 
     config.tap('webpack', (...args) => {
-        if (process.env.WEBPACK_TARGET === 'node') {
+        const [webpackConfig, , env = {}] = args;
+
+        const IS_SERVER = env.target === 'node';
+
+        if (IS_SERVER) {
             require('./lib/webpack.server')(...args);
         } else {
             require('./lib/webpack.client')(...args);
+        }
+
+
+        if (webpackConfig.plugins.has('define')) {
+            webpackConfig.plugin('define')
+                .tap(([options]) => {
+                    return [
+                        Object.assign(options, {
+                            __SERVER__: IS_SERVER
+                        })
+                    ];
+                });
+        } else {
+            webpackConfig
+                .plugin('define')
+                .use(webpack.DefinePlugin, [{
+                    __PRODUCTION__: !!env.production,
+                    __SERVER__: webpackConfig.get('target') === 'node'
+                }]);
         }
     });
 
     config.set('middlewares', ({ paths, address }) => {
         return [
             proxy({
-                host: `http://${address}:8080`,
+                host: `http://${address}:${port}`,
                 match: /(\.html?|\/)$/
             }),
             staticMiddleware(paths.toAbsPath('dist.root'))
