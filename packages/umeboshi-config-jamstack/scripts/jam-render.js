c02c0d4656652357edb7efbbae8b53d4232b857d
Marco Solazzi <marco.solazzi@gmail.com>
2018-06-21 15:55:40 +0900

    feat(config-jamstack): dev and bundle renderer

---
+++ packages/umeboshi-config-jamstack/scripts/jam-render.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const globby = require('globby');
+const makeDir = require('make-dir');
+const fs = require('fs');
+const { getTemplate } = require('../lib/utils');
+
+const { resolveConfig } = require('umeboshi-dev-utils');
+const createConfig = require('umeboshi-dev-utils/lib/config');
+
+const { api } = resolveConfig(createConfig({})).evaluate();
+
+let render;
+
+if (process.env.SSR) {
+    render = require(process.env.SSR).render; //eslint-disable-line prefer-destructuring
+}
+
+const templatePath = api.paths.toAbsPath('src.root/templates/');
+const manifest = process.env.MANIFEST && require(process.env.MANIFEST);
+
+
+globby.sync(
+    ['**/*.js'],
+    { cwd: api.paths.toAbsPath('src.assets/js/pages') }
+).forEach((page) => {
+
+    const {
+        html,
+        head = {},
+        template = 'default'
+    } = render({ path: page });
+
+    const pageTmpl = getTemplate(template, templatePath);
+
+    const output = pageTmpl({
+        html,
+        head,
+        assets: manifest
+    });
+
+    const outPath = api.paths.toAbsPath(`dist.root/${page.replace(/\.js$/, '.html')}`);
+
+    makeDir.sync(path.dirname(outPath));
+
+    fs.writeFileSync(
+        outPath,
+        output,
+        { encoding: 'utf8' }
+    );
+});
