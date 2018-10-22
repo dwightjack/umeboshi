360c2e8a0b46f39fd0e7d4717cf900f15b8ecaf1
Marco Solazzi <marco.solazzi@gmail.com>
2018-10-22 17:10:02 +0900

    chore(config-react): review

--- packages/umeboshi-config-react/index.js
+++ packages/umeboshi-config-react/index.js
@@ -1,15 +1,9 @@
 module.exports = (config) => {
-
     const jest = require('./jest.config');
 
-    config
-        .tap('jest', jest)
-        .tap('webpack', (webpackConfig) => {
-            webpackConfig.resolve.extensions
-                .merge(['.js', '.jsx', '.json']);
+    config.tap('jest', jest).tap('webpack', (webpackConfig) => {
+        webpackConfig.resolve.extensions.merge(['.js', '.jsx', '.json']);
 
-            webpackConfig.module
-                .rule('js')
-                .test(/\.jsx?$/);
-        });
+        webpackConfig.module.rule('js').test(/\.jsx?$/);
+    });
 };
--- packages/umeboshi-config-react/jest.config.js
+++ packages/umeboshi-config-react/jest.config.js
@@ -1,3 +1,4 @@
-module.exports = (jestConfig) => Object.assign(jestConfig, {
-    setupFiles: [require.resolve('./lib/jest.setup')]
-});
+module.exports = (jestConfig) =>
+    Object.assign(jestConfig, {
+        setupFiles: [require.resolve('./lib/jest.setup')]
+    });
