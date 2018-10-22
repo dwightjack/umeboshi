1810f88a52132a86aeb786791e29ee334da6e5d0
Marco Solazzi <marco.solazzi@gmail.com>
2018-10-22 17:07:49 +0900

    feat(scripts): revert to webpack-dev-server

--- packages/umeboshi-scripts/dev-server.js
+++ packages/umeboshi-scripts/dev-server.js
@@ -1,13 +1,20 @@
 const webpack = require('webpack');
 const portfinder = require('portfinder');
-const serve = require('webpack-serve');
+const serve = require('webpack-dev-server');
 const {
-    loadUmeboshiConfig, mergeConfig, evaluate, resolveConfig
+    loadUmeboshiConfig,
+    mergeConfig,
+    evaluate,
+    resolveConfig
 } = require('umeboshi-dev-utils');
 const logger = require('umeboshi-dev-utils/lib/logger');
 const createConfig = require('umeboshi-dev-utils/lib/config');
 
-const webpackConfig = require('./webpack')({ analyze: false, production: false, server: true });
+const webpackConfig = require('./webpack')({
+    analyze: false,
+    production: false,
+    server: true
+});
 let clientConfig;
 
 if (Array.isArray(webpackConfig)) {
@@ -23,7 +30,10 @@ if (!clientConfig) {
 const compiler = webpack(clientConfig);
 
 const env = {
-    analyze: false, production: false, server: true, compiler
+    analyze: false,
+    production: false,
+    server: true,
+    compiler
 };
 const { config, api } = resolveConfig(createConfig(env)).evaluate();
 const { middlewares, devServer } = config;
@@ -31,35 +41,36 @@ const { port } = api.hosts.local;
 
 //get the port and start the server
 portfinder.getPortPromise({ port }).then((p) => {
+    const {
+        devServer: umeDevServer,
+        middlewares: umeMiddlewares
+    } = loadUmeboshiConfig();
 
-    const { devServer: umeDevServer, middlewares: umeMiddlewares } = loadUmeboshiConfig();
-
-    const appMiddlewares = umeMiddlewares ? evaluate(umeMiddlewares, evaluate(middlewares, env), env) : evaluate(middlewares, env);
-    const args = [{ port: p, publicPath: webpackConfig.output.publicPath }, webpackConfig, compiler];
+    const appMiddlewares = umeMiddlewares
+        ? evaluate(umeMiddlewares, evaluate(middlewares, env), env)
+        : evaluate(middlewares, env);
+    const args = [
+        { port: p, publicPath: webpackConfig.output.publicPath },
+        webpackConfig,
+        compiler
+    ];
     const options = Object.assign(
         {
-            add(app) {
+            setup(app) {
                 if (appMiddlewares && appMiddlewares.length > 0) {
-                    appMiddlewares.forEach(
-                        (middleware) => app.use(middleware)
-                    );
+                    appMiddlewares.forEach((middleware) => app.use(middleware));
                 }
-
             }
         },
-        mergeConfig(devServer(...args), umeDevServer, ...args),
-        { compiler }
+        mergeConfig(devServer(...args), umeDevServer, ...args)
     );
 
-
-    api.hooks.devServer.promise(options, { port: p }, api)
-        .then(() => serve(options))
-        .then((server) => {
-            server.on('listening', () => {
-                logger.message('`\nStarted a server at:\n');
-                logger.message(`- http://localhost:${p}`);
-                logger.message(`- http://${api.address}:${p}\n`);
-            });
+    api.hooks.devServer.promise(options, { port: p }, api).then(() => {
+        const server = serve(compiler, options);
+        server.on('listening', () => {
+            logger.message('`\nStarted a server at:\n');
+            logger.message(`- http://localhost:${p}`);
+            logger.message(`- http://${api.address}:${p}\n`);
         });
-
+    });
 });
