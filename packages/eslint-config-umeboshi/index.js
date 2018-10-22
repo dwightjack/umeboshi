f9b893f06470672f24b2457bdf8837381169cba1
Marco Solazzi <marco.solazzi@gmail.com>
2018-10-22 13:52:13 +0900

    feat(eslint): update config to support eslint 5

--- packages/eslint-config-umeboshi/index.js
+++ packages/eslint-config-umeboshi/index.js
@@ -12,18 +12,23 @@ module.exports = {
     extends: ['eslint-config-airbnb-base'],
 
     env: {
-        commonjs: true,
         es6: true,
         browser: true
     },
 
-    settings: webpackConfPath ? {
-        'import/resolver': {
-            webpack: {
-                config: webpackConfPath
-            }
-        }
-    } : {},
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+
+    settings: webpackConfPath
+        ? {
+              'import/resolver': {
+                  webpack: {
+                      config: webpackConfPath
+                  }
+              }
+          }
+        : {},
 
     globals: {
         Modernizr: true,
@@ -31,7 +36,7 @@ module.exports = {
     },
 
     rules: {
-        indent: [2, 4],
+        indent: [2, 2],
         'no-multiple-empty-lines': [2, { max: 3 }],
         'spaced-comment': 0,
         'comma-dangle': [2, 'never'],
@@ -39,7 +44,8 @@ module.exports = {
         'padded-blocks': [1, { classes: 'always' }],
         'max-len': 0,
         'no-underscore-dangle': 0,
-        'arrow-parens': 0,
+        'arrow-parens': [1, 'always'],
+        'arrow-body-style': 0,
         'import/no-unresolved': [2, { ignore: ['[a-z]+.s?css$'] }]
     }
 };
