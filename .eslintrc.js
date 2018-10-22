4590af7744c4e07e0040ff56528de7a66d91d05b
Marco Solazzi <marco.solazzi@gmail.com>
2018-10-22 13:26:24 +0900

    style: prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,35 +8,32 @@ const airbnb = [
 ].map(require.resolve);
 
 module.exports = {
-
     root: true,
 
-    plugins: ['node'],
+    plugins: ['node', 'prettier'],
 
     env: {
         node: true,
         es6: true
     },
 
-    extends: [
-        'eslint:recommended',
-        'plugin:node/recommended'
-    ].concat(airbnb),
+    extends: ['eslint:recommended', 'plugin:node/recommended'].concat(),
+
+    parserOptions: {
+        ecmaVersion: 2018
+    },
 
     rules: {
-        indent: ['error', 4],
         'consistent-return': 'error',
         'arrow-body-style': 'off',
-        'no-multiple-empty-lines': ['warn', {'max': 3}],
+        'no-multiple-empty-lines': ['warn', { max: 3 }],
         'comma-dangle': ['error', 'never'],
         'padded-blocks': 'off',
         'arrow-parens': ['error', 'always'],
         'spaced-comment': 'off',
         'object-curly-spacing': ['error', 'always'],
         'global-require': 'off',
-        'eol-last': ['warn', 'never'],
-        'max-len': 'off',
         'prefer-template': 'off',
         'function-paren-newline': 'off'
     }
-}
+};
