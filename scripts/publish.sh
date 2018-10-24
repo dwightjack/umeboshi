d0c33c4fb18ed195452039d77a51e870b4d40cfd
Marco Solazzi <marco.solazzi@gmail.com>
2018-10-24 13:44:48 +0900

    chore: publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,15 @@
+cd packages
+npm publish babel-preset-umeboshi --tag=next
+npm publish babel-preset-umeboshi-react --tag=next
+npm publish eslint-config-umeboshi --tag=next
+npm publish eslint-config-umeboshi-node --tag=next
+npm publish eslint-config-umeboshi-react --tag=next
+npm publish eslint-config-umeboshi-vue --tag=next
+npm publish stylelint-config-umeboshi --tag=next
+npm publish umeboshi-config --tag=next
+# npm publish umeboshi-config-jamstack --tag=next
+npm publish umeboshi-config-react --tag=next
+npm publish umeboshi-config-spa --tag=next
+npm publish umeboshi-config-vue --tag=next
+npm publish umeboshi-dev-utils --tag=next
+npm publish umeboshi-scripts --tag=next
